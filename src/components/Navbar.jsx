acc1d1167f09669a0e90212fb5f513f3de01ef54
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-10-22 14:23:32 -0500

    i dont even know

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div className="nav__container">
+        <div className="logo">Movie List</div>
+
+    </div>
+    <>
+    <ul>
+        <li>
+
+        </li>
+    </ul>
+</>
+  )
+}
+
+export default Navbar
