5302c71305c9e80ac27af03092c98901310a1f10
mmcmullinyohe <mmcmullinyohe@gmail.com>
2025-10-22 13:25:47 -0500

    home and movie

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
+import  { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+      <Router>
+        <div className="App">
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route></Route>
+          <Route></Route>
+        </Routes>
+        </div>
+      </Router>
+    
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      Testing Home
+    </div>
+  )
+}
+
+export default Home
